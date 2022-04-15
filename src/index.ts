7f4fdc7a6effd005ef1337c66df4160f2ae09199
atrush <atrush.main@gmail.com>
2022-04-15 20:33:37 +0300

    Update index file for Google Cloud Function usage

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,7 @@ import config from './config';
 import logger from './infrastructure/logger';
 import StatementExporter from './exporter/StatementExporter';
 
-const EXIT_STATUS_SUCCESS = 0;
-const EXIT_STATUS_ERROR = 1;
-
-async function main() {
+export async function exportStatement() {
     try {
         const { bankName, maxMonthsToExport, timeZone } = config;
 
@@ -17,13 +14,7 @@ async function main() {
         await statementExporter.run();
 
         logger.info('Completed statement export successfully');
-
-        process.exit(EXIT_STATUS_SUCCESS);
     } catch (error) {
         logger.error('Failed statement export process', { reason: error.message });
-
-        process.exit(EXIT_STATUS_ERROR);
     }
 }
-
-main();
